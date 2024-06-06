a45614ecd0c0a4275b4e545eb1cd689455cd6af3
SpyOfAlien <doanquangbao.it@gmail.com>
2024-06-06 22:06:46 +0700

    get everything up and running

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 var mnemonic =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+  "maximum dirt panda area wide toast marble music oven office result case";
 
 module.exports = {
   networks: {
@@ -9,12 +9,12 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: "*",
-      gas: 9999999,
+      gas: 6721975,
     },
   },
   compilers: {
     solc: {
-      version: "^0.4.24",
+      version: "^0.4.25",
     },
   },
 };
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -21,6 +21,9 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimize: false,
+  },
   plugins: [
     new StartServerPlugin("server.js"),
     new webpack.NamedModulesPlugin(),
